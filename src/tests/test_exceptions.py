8dcc020fb94262ee363e8932327899bd38e7ebdf
dmitriy-ga <Dmitriy2243@yandex.ru>
2023-01-17 13:32:56 +0500

    Add tests for 404 error and non-existent folder handling

---
+++ src/tests/test_exceptions.py
@@ -0,0 +1,21 @@
+import requests.exceptions
+from src.page_loader import get_main_page, check_folder_exist
+import pytest
+import tempfile
+import os.path
+
+
+def test_error_code(requests_mock):
+    test_address: str = 'https://testdownload.net/notexistpage'
+    requests_mock.get(test_address, status_code=404)
+    with pytest.raises(requests.exceptions.HTTPError) as e:
+        get_main_page(test_address)
+    assert e.type == requests.exceptions.HTTPError
+
+
+def test_non_existent_folder():
+    with tempfile.TemporaryDirectory() as d:
+        test_path = os.path.join(d, 'some/random/path')
+        with pytest.raises(ValueError) as e:
+            check_folder_exist(test_path)
+        assert e.type == ValueError
