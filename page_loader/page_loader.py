c6a36b9f92cd30ac15f5a86250be5fed25e2ad7e
dmitriy-ga <Dmitriy2243@yandex.ru>
2023-02-06 16:47:21 +0500

    Refactor: main function fragmented
    In download() work of downloading and handling of resources now processing by get_resources() and download_assets

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,10 +1,10 @@
 import logging
 import os
-from typing import NamedTuple
 import bs4
 import requests
 from progress.bar import Bar
-from .file_processor import build_url_info, build_item_info, ItemInfo, LINK
+from .file_processor import build_url_info, build_item_info,\
+    UrlInfo, ItemInfo, LINK
 
 IMG = 'img'
 SCRIPT = 'script'
@@ -19,57 +19,77 @@ def check_folder_exist(folder_path: str) -> None:
     logging.info('Folder founded')
 
 
-def get_main_page(url: str) -> requests.Response:
+def get_main_page(url: str) -> bs4.BeautifulSoup:
     logging.debug(f'Getting response from {url}')
     response: requests.Response = requests.get(url)
     response.raise_for_status()
     logging.info('Response passed')
-    return response
+    return bs4.BeautifulSoup(response.text, 'html.parser')
 
 
-def prepare_output_folder(full_path: str, output_path: str) -> None:
+def prepare_output_folder(full_path: str) -> None:
     if not os.path.exists(full_path):
         logging.info(f'Creating folder {full_path}')
         try:
             os.mkdir(full_path)
         except OSError:
-            raise OSError(f'Unable create folder at {output_path}')
+            raise OSError(f'Unable create folder at {full_path}')
 
 
-def download(url: str, output_path: str) -> str:
-    check_folder_exist(output_path)
-    url_names: NamedTuple = build_url_info(url, output_path)
+def get_resources(url: str, url_names: UrlInfo
+                  ) -> (bs4.BeautifulSoup, list[ItemInfo, ...]):
 
-    response: requests.Response = get_main_page(url)
-    soup: bs4.BeautifulSoup = bs4.BeautifulSoup(response.text, 'html.parser')
+    soup: bs4.BeautifulSoup = get_main_page(url)
 
-    assets: list[bs4.ResultSet] = (soup.find_all(IMG)
-                                   + soup.find_all(LINK)
-                                   + soup.find_all(SCRIPT, src=True)
-                                   )
-    if len(assets):
-        prepare_output_folder(url_names.directory_full_path, output_path)
+    assets: list[bs4.Tag, ...] = (soup.find_all(IMG)
+                                  + soup.find_all(LINK)
+                                  + soup.find_all(SCRIPT, src=True)
+                                  )
 
-    for item in Bar('Downloading').iter(assets):
+    assets_actual: list[ItemInfo, ...] = []
 
+    # Updating all links in HTML:
+    for item in assets:
         item_names: ItemInfo = build_item_info(item, url)
 
-        # Checking same host of item
-        if all((not item_names.item_host == '',
-                not item_names.item_host == url_names.host_url)):
+        if not is_same_host(item_names.item_host, url_names.host_url):
             logging.info(f'{item_names.name} skipped, non-same host')
             continue
 
         full_name: str = os.path.join(url_names.folder_name, item_names.name)
-
-        item_content = requests.get(item_names.item_url).content
-
         # Updating local HTML file for new address
         item[item_names.item_url_index] = full_name
+        assets_actual.append(item_names)
+    return soup, assets_actual
 
-        logging.debug(f'Saving resource {item_names.name}')
+
+def is_same_host(url_item: str, url_host: str) -> bool:
+    if url_item == '':
+        url_item = url_host
+    return url_item == url_host
+
+
+def download_assets(assets: list[ItemInfo],
+                    url_names: UrlInfo, output_path: str) -> None:
+    if not len(assets):
+        return None
+
+    prepare_output_folder(url_names.directory_full_path)
+
+    for item in Bar('Downloading').iter(assets):
+        item_content = requests.get(item.item_url).content
+        full_name: str = os.path.join(url_names.folder_name, item.name)
+        logging.debug(f'Saving resource {item.name}')
         with open(os.path.join(output_path, full_name), WB) as file:
             file.write(item_content)
+
+
+def download(url: str, output_path: str) -> str:
+    check_folder_exist(output_path)
+    url_names: UrlInfo = build_url_info(url, output_path)
+
+    soup, assets_actual = get_resources(url, url_names)
+    download_assets(assets_actual, url_names, output_path)
 
     logging.debug(f'Saving HTML page {url_names.html_name}')
     with open(url_names.html_full_path, W) as file:
