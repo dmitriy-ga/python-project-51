9a3db88044a1b9e49b1c1cf2f3ed41ab5464ca62
dmitriy-ga <Dmitriy2243@yandex.ru>
2023-03-01 13:32:13 +0500

    Refactor: use parametrize in tests

--- page_loader/tests/test_page_loader.py
+++ page_loader/tests/test_page_loader.py
@@ -6,40 +6,61 @@ import os
 import logging
 
 
-@pytest.fixture
-def text_simple() -> str:
-    file_example_path = 'page_loader/tests/fixtures/simple_text.html'
-    expected = read_file(file_example_path)
-    return expected
-
-
-def read_file(file_path) -> str:
+def read_html(file_path: str) -> str:
     with open(file_path) as file:
-        file_content = file.read()
-    return file_content
+        return file.read()
 
 
-def mock_request_text(address, fixture, directory):
-    with requests_mock.Mocker() as m:
-        m.get(address, text=fixture)
-        received_mock_path: str = download(address, directory)
-    return received_mock_path
+def read_binary_file(file_path: str) -> bytes:
+    with open(file_path, 'rb') as file:
+        return file.read()
 
 
-def test_download(caplog, text_simple) -> None:
+site_url: str = 'https://testdownload.net/'
+fixtures_path: str = 'page_loader/tests/fixtures/'
+
+empty_link = read_binary_file(os.path.join(fixtures_path, 'empty_link.css'))
+simple_link = read_html(os.path.join(fixtures_path, 'simple_link.html'))
+
+empty_script = read_binary_file(os.path.join(fixtures_path, 'empty_script.js'))
+simple_script = read_html(os.path.join(fixtures_path, 'simple_script.html'))
+
+example_pic = read_binary_file(os.path.join(fixtures_path, 'example_pic.jpg'))
+simple_pic = read_html(os.path.join(fixtures_path, 'simple_pic.html'))
+
+example_text = read_binary_file(os.path.join(fixtures_path, 'simple_text.html'))
+simple_text = read_html(os.path.join(fixtures_path, 'simple_text.html'))
+
+
+@pytest.mark.parametrize(
+    'fixture, fixture_html, fixture_url, item_path', [
+        (empty_link, simple_link,
+         'https://testdownload.net/empty_link.css',
+         'testdownload-net_files/testdownload-net-empty-link.css', ),
+
+        (empty_script, simple_script,
+         'https://testdownload.net/empty_script.js',
+         'testdownload-net_files/testdownload-net-empty-script.js'),
+
+        (example_pic, simple_pic,
+         'https://testdownload.net/example_pic.jpg',
+         'testdownload-net_files/testdownload-net-example-pic.jpg'),
+
+        (example_text, simple_text,
+         None,
+         'testdownload-net.html')
+    ])
+def test_download_page(caplog, fixture, fixture_html, fixture_url, item_path
+                       ) -> None:
+
     caplog.set_level(logging.DEBUG)
-    test_address: str = 'https://testdownload.net/page'
-    test_name: str = 'testdownload-net-page.html'
-
     with tempfile.TemporaryDirectory() as d:
         logging.debug(f'Created temporary folder {d}')
 
-        received_mock_path: str = mock_request_text(test_address,
-                                                    text_simple,
-                                                    d)
+        with requests_mock.Mocker() as m:
+            m.get(site_url, text=fixture_html)
+            m.get(fixture_url, content=fixture)
+            download(site_url, d)
+        received_file: bytes = read_binary_file(os.path.join(d, item_path))
 
-        received_mock: str = read_file(received_mock_path)
-        assert received_mock == text_simple
-
-        _, received_name = os.path.split(received_mock_path)
-        assert received_name == test_name
+    assert received_file == fixture
